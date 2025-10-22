ddcc10fd8e35edff35ebdbc330eb21e79d522173
Thomas Dubos <dubos@lmd.polytechnique.fr>
2025-10-22 10:05:57 +0200

    Restore GeoMakie plots (#33)
    Solves issue https://github.com/ClimFlows/ClimFlowsPlots.jl/issues/32

--- src/voronoi_sphere.jl
+++ src/voronoi_sphere.jl
@@ -1,12 +1,5 @@
 module VoronoiSphere
 
-# import GeometryBasics as GB
-# import Makie
-# using GeoMakie
-# using ColorSchemes
-
-# import ..SphericalInterpolations as SI
-
 const DATA_ON_PRIMAL = "Data must be a scalar defined at primal cells of a spherical Voronoi mesh and passed as a Makie.Observable"
 
 """
@@ -45,50 +38,5 @@ function plot_orthographic end # implemented in extension
     primal cell centers (which coincides with triangle vertices).
 """
 function plot_native_3D end # implemented in extension
-
-#=
-function plot_2D(sphere, data::Makie.Observable; resolution = 1.0, options...)
-    # check that data is on primal mesh and interpolate to lon-lat
-    @assert length(data[]) == length(sphere.Ai)
-    lons, lats = -180:resolution:180, -90:resolution:90
-    color = map(transpose ∘ SI.lonlat_interp(sphere, lons, lats), data)
-    # create and return plot
-    fig = Makie.Figure(size = (1440, 720))
-    ax = Makie.Axis(fig[1, 1], aspect = 2)
-    Makie.heatmap!(ax, color; colormap = :hot, options...)
-    return fig
-end
-
-function plot_orthographic(sphere, data::Makie.Observable; resolution = 0.5, options...)
-    # check that data is on primal mesh and interpolate to lon-lat
-    @assert length(data[]) == length(sphere.Ai)
-    lons, lats = -180:resolution:180, -90:resolution:90
-    color = map(transpose ∘ SI.lonlat_interp(sphere, lons, lats), data)
-    # create and return plot
-    fig = Figure()
-    ga = GeoAxis(fig[1, 1]; dest = "+proj=ortho +lon_0=19 +lat_0=50")
-    surface!(ga, lons, lats, color; shading = NoShading, options...)
-    lines!(ga, GeoMakie.coastlines())
-    return fig
-end
-
-function plot_native_3D(sphere, data::Makie.Observable; zoom = 1.6, options...)
-    # check that data is on primal mesh
-    @assert length(data[]) == length(sphere.Ai)
-    # build graphical mesh
-    lon, lat, vertex = sphere.lon_i, sphere.lat_i, sphere.dual_vertex
-    xyz(lon, lat) = cos(lat) * cos(lon), cos(lat) * sin(lon), sin(lat)
-    nodes = [GB.Point3f(xyz(lon[i], lat[i])) for i in eachindex(lon)]
-    faces = [
-        GB.GLTriangleFace((vertex[1, i], vertex[2, i], vertex[3, i])) for
-        i in axes(vertex, 2)
-    ]
-    makiemesh = GB.Mesh(nodes, faces)
-    # create and return plot
-    fig, ax, obj = Makie.mesh(makiemesh; color = data, options...)
-    Makie.scale!(ax.scene, zoom, zoom, zoom)
-    return fig
-end
-=#
 
 end # VoronoiSphere
